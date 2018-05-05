dc138eccc84aa622219378332c99774d4f19d54d
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-05 18:52:10 -0400

    add new and create methods to controllers and forms to views for groups

--- app/controllers/cohorts_controller.rb
+++ app/controllers/cohorts_controller.rb
@@ -8,9 +8,16 @@ class CohortsController < ApplicationController
     end
 
     def new
+        @cohort = Cohort.new
     end
 
     def create
+        @cohort = Cohort.new(cohort_params)
+        if @cohort.save
+            redirect_to(cohorts_path)
+        else
+            render('new')
+        end
     end
 
     def edit
@@ -23,5 +30,11 @@ class CohortsController < ApplicationController
     end
 
     def destroy
+    end
+
+    private
+
+    def cohort_params
+        params.require(:cohort).permit(:title, :start_date, :end_date, :employee_id)
     end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -8,9 +8,16 @@ class EmployeesController < ApplicationController
     end
 
     def new
+        @employee = Employee.new
     end
 
     def create
+        @employee = Employee.new(employee_params)
+        if @employee.save
+            redirect_to(employee_path)
+        else
+            render('new')
+        end
     end
 
     def edit
@@ -23,5 +30,11 @@ class EmployeesController < ApplicationController
     end
 
     def destroy
+    end
+    
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :position, :salary, :age, :education, :email, :phone_number, :password_digest)
     end
 end
