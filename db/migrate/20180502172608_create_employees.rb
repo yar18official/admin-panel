1b516cd2cff98bebdfd82bc6f06a0316504aeaf2
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-08 15:47:54 -0400

    add User views/models/db migrations with Devise gem

--- db/migrate/20180502172608_create_employees.rb
+++ db/migrate/20180502172608_create_employees.rb
@@ -3,13 +3,12 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
         create_table :employees do |t|
             t.string :first_name
             t.string :last_name
-            t.integer :age
+            t.date :birthday
             t.integer :salary
             t.string :position
             t.string :education
             t.string :phone_number
             t.string :email
-            t.string :password_digest
 
             t.timestamps
         end
--- db/migrate/20180502173210_create_students.rb
+++ db/migrate/20180502173210_create_students.rb
@@ -3,7 +3,7 @@ class CreateStudents < ActiveRecord::Migration[5.2]
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-      t.integer :age
+      t.date :birthday
       t.string :education
       t.string :email
       t.string :phone_number
