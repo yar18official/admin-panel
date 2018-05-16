25d5f9f9f455060da7123f0ea345974f319901e2
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-15 20:33:19 -0400

    fix class/cohort roster views

--- app/controllers/course_registrations_controller.rb
+++ app/controllers/course_registrations_controller.rb
@@ -1,4 +1,7 @@
 class CourseRegistrationsController < ApplicationController
+    load_and_authorize_resource
+    before_action :authenticate_user!
+    before_action :set_course_registration, only: [:show, :edit, :update, :destroy]
 
     def new
         @course_registration = CourseRegistration.new
@@ -16,11 +19,9 @@ class CourseRegistrationsController < ApplicationController
     end
 
     def edit
-        @course_registration = CourseRegistration.find(params[:id])
     end
 
     def update
-        @course_registration = CourseRegistration.find(params[:id])
         if @course_registration.update_attributes(cohort_params)
             @course = Course.find(@course_registration[:course_id])
             flash[:notice] = "Course '#{@course.title}' roster updated successfully"
@@ -31,11 +32,9 @@ class CourseRegistrationsController < ApplicationController
     end
 
     def delete
-        @course_registration = CourseRegistration.find(params[:id])
     end
 
     def destroy
-        @course_registration = CourseRegistration.find(params[:id])
         @course_registration.destroy
         @student = Student.find(@course_registration[:student_id])
         flash[:notice] = "'#{@student.first_name} #{@student.last_name}' was removed"
@@ -43,8 +42,10 @@ class CourseRegistrationsController < ApplicationController
     end
 
     private
-
-    def course_registration_params
-        params.require(:course_registration).permit(:course_id, :student_id)
-    end
+        def set_course_registration
+            @course_registration = CourseRegistration.find(params[:id])
+        end
+        def course_registration_params
+            params.require(:course_registration).permit(:course_id, :student_id)
+        end
 end
