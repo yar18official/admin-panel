f95cfde6541364738d03d2e02b91e01fd7d16d8e
Danielle Danskin <danielle.danskin@gmail.com>
2018-05-22 17:11:18 -0400

    fix delete function for cohorts, courses and students

--- db/migrate/20180513202443_create_course_registrations.rb
+++ db/migrate/20180513202443_create_course_registrations.rb
@@ -1,9 +1,8 @@
 class CreateCourseRegistrations < ActiveRecord::Migration[5.2]
   def change
     create_table :course_registrations do |t|
-      t.integer :course_id
-      t.integer :student_id
-
+      t.references :course_id, foreign_key: true
+      t.references :student_id, foreign_key: true
       t.timestamps
     end
     add_index("course_registrations", ['course_id', 'student_id'])
