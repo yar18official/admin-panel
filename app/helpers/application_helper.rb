ca4a4c9f2ee5ce426b858138c63e7af55e887d1d
Danielle Danskin <danielle.danskin@gmail.com>
2018-06-01 13:32:59 -0400

    fix updating of student roster

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,7 @@ module ApplicationHelper
     def get_registration(course_id, student_id)
         return CourseRegistration.find_by(course_id: course_id, student_id: student_id)
     end
+    def registration_record(student_id, course_id)
+        CourseRegistration.find(student_id: student_id, course_id: course_id)
+    end
 end
--- app/models/course_registration.rb
+++ app/models/course_registration.rb
@@ -2,5 +2,5 @@ class CourseRegistration < ApplicationRecord
     include PublicActivity::Model
     tracked owner: Proc.new{ |controller, model| controller.current_user }
     belongs_to :course
-    belongs_to :student
+    belongs_to :student 
 end
